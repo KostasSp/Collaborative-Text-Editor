0208a211b42a92ebe0494b60558651ceef4198f2
Kostas <kostasspanolios@gmail.com>
2022-03-21 09:20:30 +0000

    server accepts instance id and sends changes to clients with the same id

--- server/Server.js
+++ server/Server.js
@@ -6,6 +6,11 @@ const io = require("socket.io")(3001, {
 });
 
 io.on("connection", (socket) => {
+  socket.on("get-instance", (id) => {
+    const data = null;
+    socket.join(id);
+    socket.emit("load-instance", data);
+  });
   socket.on("send-change", (delta) => {
     //broadcasting to all client instances
     socket.broadcast.emit("receive-change", delta);
